76f6dd04d000cb89715879605cc4e60484d69cfe
shalukap <jude.shaluka.perera@gmail.com>
2025-09-29 08:12:54 +0530

    change chrome path

--- app/Http/Controllers/CollectionReportController.php
+++ app/Http/Controllers/CollectionReportController.php
@@ -22,7 +22,7 @@ class CollectionReportController extends Controller
        $payments=Payment::when($startDate, fn($q) => $q->whereDate('payment_date', '>=', $startDate))
                       ->when($endDate, fn($q) => $q->whereDate('payment_date', '<=', $endDate))->with('feeAssignment.student')->latest()->get();
        $report= view('reports.collectionreport',compact('payments'))->render();      
-        $pdf=Browsershot::html($report)->setChromePath('/usr/bin/chromium-browser')->noSandbox()->format('A4')->margins(5, 5, 5, 5)->pdf();
+        $pdf=Browsershot::html($report)->setChromePath('/usr/bin/google-chrome')->noSandbox()->format('A4')->margins(5, 5, 5, 5)->pdf();
        
        return response()->make($pdf, 200, [
                 'Content-Type' => 'application/pdf',
